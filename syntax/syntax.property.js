3a5124b653d8bf4d69b18ceeeca6017a2d1777e0
satoshi okami <satoshi@okami-satoshi-no-MacBook-Pro.local>
2013-01-08 01:57:25 +0900

    update property syntax of babe language

---
+++ syntax/syntax.property.js
@@ -0,0 +1,48 @@
+
+ definition of property
+=========================
+
+a
+// Variable declaration　error, variable has to be initialized.
+
+
+a = None
+// var a = null
+
+
+a = 1
+// var a = 1;
+
+
+a = "aa"
+// var a = "aa";
+
+
+a = NaN
+// var a = NaN;
+
+
+a = Infinity
+// var a = Infinity;
+
+
+a = true
+// var a = true;
+
+
+a = false
+// var a = false;
+
+
+/* unimplemented */
+a = 1
+b = "{a} + 1 = 2"
+// var a = 1;
+// var b = "1 + 1 = 2";
+
+
+/* unimplemented */
+a = 1
+b = '{a} + 1 = 2'
+// var a = 1;
+// var b = '1 + 1 = 2';
